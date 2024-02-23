6ded5d65adfbf5748725f20ac55c7a34adba1aff
Paúl Jheferson Guevara Linares <pguevarl@emeal.nttdata.com>
2024-02-23 12:26:05 -0500

    fix

--- spring-boot-webflux-apirest_3_validation/spring-boot-webflux-apirest/src/main/java/com/bolsadeideas/springboot/webflux/app/models/dao/CategoriaDao.java
+++
@@ -1,9 +0,0 @@
-package com.bolsadeideas.springboot.webflux.app.models.dao;
-
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-import com.bolsadeideas.springboot.webflux.app.models.documents.Categoria;
-
-public interface CategoriaDao extends ReactiveMongoRepository<Categoria, String>{
-
-}
